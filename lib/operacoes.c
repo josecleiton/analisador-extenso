51932c99b34b55a187c6bdeb1dbf389892800b6d
jcleitonbc <jcleitonbc@gmail.com>
2018-04-21 11:30:46 -0300

    Função soma adicionada e funcional, lembrar de tratar função inverte!

--- lib/operacoes.c
+++ lib/operacoes.c
@@ -6,6 +6,8 @@
 char* multiplica (char a[], char b[]);
 char* soma (char a[], char b[]);
 char* char2int (char a[]);
+char* int2char (char a[]);
+int inverte (char a[]);
 int maior (int a, int b);
 int menor (int a, int b);
 
@@ -19,10 +21,23 @@ int main()
     printf("Digite o segundo numero: ");
     scanf("%s",b);
     printf("Soma dos dois numeros: ");
-    //soma (a, b);
-    c = multiplica(a,b);
-    printf("%s\n",c);
+    c = soma (a, b);
+   // c = multiplica(a,b);
+    printf("%s\n", c);
     return 0;
+}
+
+int inverte (char a[]) //AJUSTAR ESSA FUNÇÃO, PARA QUE OS ZEROS A DIREITA FIQUEM NA ESQUERA!
+{
+    int i, tam = strlen (a);
+    char c;
+    for (i=0; i<tam/2; i++)
+    {
+        c = a[i];
+        a[i] = a[tam-1-i];
+        a[tam-1-i] = c;
+    }
+    return 1;
 }
 
 char* char2int (char a[])
@@ -33,25 +48,69 @@ char* char2int (char a[])
     return a;
 }
 
+char* int2char (char a[])
+{
+    int i, tam = strlen (a);
+    for (i=0; i<=tam; i++)
+        a[i] = a[i] + '0';
+    return a;
+}
+
 int maior (int a, int b)
 {
-    return (a>b)*a + (b>a)*b;
+    return (a>=b)*a + (b>a)*b;
 }
 
 int menor (int a, int b)
 {
-    return (a<b)*a + (b<a)*b;
+    return (a<=b)*a + (b<a)*b;
 }
 
 char* soma (char a[], char b[])
 {
-    int ta = strlen (a), tb = strlen (b), ts = maior (ta, tb) + 2; //TAMANHO DAS STRINGS
-    int i, j, k, resto;
+    int ta = strlen (a), tb = strlen (b), ts = maior (ta, tb); //TAMANHO DAS STRINGS
+    int i, resto;
     char* soma = (char*) malloc (ts);
     for (i=0; i<ts; i++)
+        soma[i] = 0;
+    inverte (a); inverte (b);
+    char2int (a); char2int (b);
+    putchar('\n');
+    //for (i=ta-1; i >= 0; i--)
+     //   printf("%d\n", a[i]);
+    for (i=0; i<menor(ta, tb); i++)
     {
-
+        soma[i] += a[i] + b[i];
+        if (i) soma[i] += resto;
+        resto = 0;
+        if (soma[i] >= 10)
+        {
+            soma[i] %= 10;
+            resto = 1; //RESTO VAI SER OU UM OU ZERO!
+        }
     }
+    if (ta == tb)
+        soma[i] = resto;
+    if (ta > tb)
+    {
+        while (i<ts)
+        {
+            soma[i] += a[i] + resto;
+            resto = 0;
+            i++;
+        }
+    }
+    else
+    {
+        while (i<ts)
+        {
+            soma[i] += b[i] + resto;
+            resto = 0;
+            i++;
+        }
+    }
+    int2char (soma);
+    return soma;
 }
 
 char* multiplica (char a[],char b[])
