fa09432c89bc98f26e6042a315c006e1ed839c12
jcleitonbc <jcleitonbc@gmail.com>
2018-05-11 16:13:22 -0300

    retira espaços

--- body.c
+++ body.c
@@ -4,18 +4,25 @@
 #endif
 #define TAM 26
 #define ARQ_ORDENS "lib/ordens.txt"
+#define ARQ_SAIDA "lib/resultados.txt"
 typedef struct ordem Ordem;
+typedef struct ocorre Ocorre;
 struct ordem
 {
     char* nome;
     char* valor;
 };
+struct ocorre
+{
+    char pos;
+    char cursor;
+};
 enum tokens
 {
     UNIT,
-    DEZ = 21,
-    CENT = 29,
-    MIL = 38,
+    DEZ = 20,
+    CENT = 28,
+    MIL = 37,
     MILHAO,
     BILHAO,
     TRILHAO,
@@ -29,8 +36,10 @@ enum tokens
     MULTI,
     DIV,
     MOD,
-    FATORIAL
+    FATORIAL /* 52 */
 };
+char flagErro;
+Ocorre flagPos[7] = {0, 0, 0, 0, 0, 0, 0}; /*2 posições para os parenteses | 4 posições para as ordens | 1 posição para a operação */
 /*
 *   FUNÇÃO QUE DEVOLVE O "DICIONÁRIO" PARA
 *   A ANÁLISE DOS NUMEROS PRESENTES DE FORMA 
@@ -59,7 +68,7 @@ Ordem* cria_dic (void)
     return ref;
 }
 
-int compara (char* s1, char* s2) //relativo a strcmp
+int compara (char* s1, char* s2) /*relativo a strcmp mas para quando encontra o espaço*/
 {
 	int i;
 	for(i=0; (s1[i]!='\0' && s1[i]!=' ') && s2[i]!='\0'; ++i)
@@ -74,9 +83,19 @@ int compara (char* s1, char* s2) //relativo a strcmp
 
 char* erroSintaxe (char* a, int cursor)
 {
-    char* erro = (char*) malloc (strlen(a)+76+cursor);
+    char* erro = (char*) malloc (strlen(a)+120+cursor);
     if (!erro) ERRO;
-    strcpy (erro, "erro de sintaxe: palavra nao faz parte de uma expressao numerica valida.\n\t");
+    strcpy (erro, "erro de sintaxe: ");
+    switch (flagErro)
+    {
+        case 1:
+        strcat (erro, "palavra nao faz parte de uma expressao numerica valida.\n\t");
+        break;
+        
+        case 2:
+        strcat (erro, "alguma abertura ou fechamento de parentese esta faltando.\n\t");
+        break;
+    }
     strcat (erro, a);
     strcat (erro, "\n\t");
     int tamErro = strlen(erro), i = 0;
@@ -90,35 +109,152 @@ char* erroSintaxe (char* a, int cursor)
     return erro;
 }
 
-char* cardinalNumeral (char a[])
+void remEspacosInicio (char* str) /*SE OS ESPAÇO VIEREM NO COMEÇO, COPIE TODA A STRING PARA O COMEÇO*/
+{
+    int i, sp = 0;
+    if (isspace (str[0]))
+    {
+        while (isspace (str[sp+1]))
+            sp++;
+        sp++;
+        for (i=0; str[sp]!='\0'; i++)
+        {
+            str[i] = str[sp];
+            sp++;
+        }
+        str[i] = '\0';
+    }
+}
+
+char* remEspacos (char* str) /* SE HOUVEREM ESPAÇOS NO MEIO E NO FIM, RETIRE-OS */
+{
+    char* vazio = (char*) malloc (strlen(str));
+    int i = 0, j = 0;
+    
+    while (str[i] != '\0') 
+    {
+        if (str[i] == ' ') 
+        {
+            int temp = i + 1;
+            if (str[temp] != '\0') 
+            {
+                while (str[temp] == ' ') 
+                {
+                    i++;
+                    temp++;
+                }
+            }
+        }
+        vazio[j] = str[i];
+        i++;
+        j++;
+    }
+    free (str);
+    vazio[j] = '\0';
+    return vazio;
+ 
+}
+
+char* cardinalNumeral (char* exps)
 {
     Ordem* ref = cria_dic ();
-    int cursor = 0, i = 0;
+    int cursor = 0, i = 0, k;
     char* flag = NULL;
+    remEspacosInicio (exps);
+    char* a = remEspacos (exps);
     char* num = (char*) malloc (strlen(a));
-    while(a[cursor]!='\0') // PROCURAR FUNCAO DA STRING.H QUE FAÇA ISSO
+
+    while(a[cursor]!='\0')
     {
         i=0;
         while (i<TAM*2)
         {
+            if (! compara (&a[cursor], (char*) "abre ")) 
+            {        
+                a[cursor+4] = '-';
+                flagPos[0].pos++;
+                flagPos[0].cursor = cursor;
+            }
+            else if (! compara (&a[cursor], (char*) "fecha "))
+            {
+                a[cursor+5] = '-';
+                flagPos[1].pos++;
+                flagPos[1].cursor = cursor;
+            }  
             if (! compara(&a[cursor], ref[i].nome))
             {
+
+                if (i>=UNIT && i<DEZ)
+                {
+                    flagPos[2].pos++;
+                    flagPos[2].cursor = cursor;
+                }
+                else if (i>=DEZ && i<CENT)
+                {
+                    flagPos[3].pos++;
+                    flagPos[3].cursor = cursor;
+                }
+                else if (i>=CENT && i<MIL)
+                {
+                    flagPos[4].pos++;
+                    flagPos[4].cursor = cursor;
+                }
+                else if (i>=MIL && i<ABRE_P)
+                {
+                    flagPos[5].pos++;
+                    flagPos[5].cursor = cursor;
+                }
                 strcat (num, ref[i].valor);
                 break;
             }
             i++;
         }
-        if (i == TAM*2) return erroSintaxe(a, cursor);
+
+
+
+        for (k=0; k<6; k++)
+        {
+            if (flagPos[k].pos > 1)
+            {
+                flagErro = 3;
+                char* erro = (char*) malloc (500);
+                if (!erro) ERRO;
+                strcpy (erro, erroSintaxe (a, (int) flagPos[k].cursor));
+            }
+        }
+
+        if (i == TAM*2)
+        {
+            flagErro = 1;
+            char* erro = (char*) malloc (500);
+            if (!erro) ERRO;
+            strcpy (erro, erroSintaxe (a, cursor));
+            puts (erro);
+            return erro;
+        }
+
         flag = strpbrk (&a[cursor], " ");
         if (! flag) break;
+
         cursor = flag + 1 - a;
     }
+
+    if (flagPos[0].pos != flagPos[1].pos)
+    {
+        flagErro = 2;
+        char* erro = (char*) malloc (500);
+        if (!erro) ERRO;
+        strcpy (erro, erroSintaxe (a, 0));
+        puts (erro);
+        return erro;
+    }
+
     return num;
 }
 
 int main (void)
 {
-    char EXP[TAM*10];
+    char* EXP = (char*) malloc (sizeof (TAM*10));
     scanf("%[^\n]", EXP);
     char* res = cardinalNumeral (EXP);
     puts (res);
