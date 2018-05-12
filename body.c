2976a087f1e2ac535945b98985ab39f24cdccc26
jcleitonbc <jcleitonbc@gmail.com>
2018-05-12 11:08:29 -0300

    True analex in coming

--- body.c
+++ body.c
@@ -1,10 +1,14 @@
 #ifndef INCLUSOS
     #define INCLUSOS
     #include "lib/preproc.h"
+    #define ERRO exit (3141592)
 #endif
-#define TAM 26
+#include "lib/operacoes.c"
 #define ARQ_ORDENS "lib/ordens.txt"
+#define ARQ_ENTRADA "lib/expressoes.txt"
 #define ARQ_SAIDA "lib/resultados.txt"
+#define ARQ_ERROS "lib/erros.txt"
+#define TAM 26
 typedef struct ordem Ordem;
 typedef struct ocorre Ocorre;
 struct ordem
@@ -31,20 +35,130 @@ enum tokens
     SEXTILHAO,
     ABRE_P,
     FECHA_P,
+    DEL_E,
     SOMA,
     SUBTRACAO,
     MULTI,
     DIV,
-    MOD,
-    FATORIAL /* 52 */
+    FATORIAL,
+    DELIMITADOR /* 52 */
 };
-char flagErro;
-Ocorre flagPos[7] = {0, 0, 0, 0, 0, 0, 0}; /*2 posições para os parenteses | 4 posições para as ordens | 1 posição para a operação */
-/*
-*   FUNÇÃO QUE DEVOLVE O "DICIONÁRIO" PARA
-*   A ANÁLISE DOS NUMEROS PRESENTES DE FORMA 
-*   EXTENSA NAS EXPRESSÕES
+
+/* 
+*
+*
+*       VARIAVEIS GLOBAIS 
+*
+*
 */
+Ordem* ref;
+char *EXP, *_TEXP;
+char token[30];
+char tipoToken;
+char strErro[300];
+
+void erroSintaxe (int tipoErro);
+void expAnaliserStart (char resultado[]);
+void expResTerms (char resultado[]); /* ROTINA QUE SOMA OU SUBTRAI TERMOS */
+void expResFator (char resultado[]);
+void get_token (void);
+Ordem* cria_dic (void);
+int compara (char* s1, char* s2);
+char* cardinalNumeral (void);
+
+int main (void)
+{
+    EXP = (char*) malloc (sizeof (TAM*10));
+    scanf("%[^\n]", EXP);
+    char* res = cardinalNumeral ();
+    puts (res);
+    return 0;
+}
+
+void expAnaliserStart (char resultado[])
+{
+    _TEXP = EXP;
+    get_token ();
+    if (!*token)
+    {
+        erroSintaxe (3);
+        return;
+    }
+    expResTerms (resultado);
+}
+
+void expResTerms (char resultado[])
+{
+    register char op;
+    char* temp;
+    expResFator (resultado);
+    while ((op=*token) == '+' || op == '-')
+    {
+        get_token();
+        expResFator (temp);
+        switch (op)
+        {
+            case '+':
+            subtrair (resultado, temp);
+            break;
+            case '-':
+            soma (resultado, temp);
+            break;
+        }
+    }
+}
+
+void expResFator (char resultado[])
+{
+
+}
+
+void get_token (void)
+{
+    register char *temp;
+    int i;
+    tipoToken = 0;
+    temp = token;
+    *temp = '\0';
+    if (!*EXP) return;
+    while (isspace (*EXP)) 
+        ++EXP;
+    int k = 0;
+    char chEXP;
+    while (EXP[k] && isalpha (EXP[k])) k++;
+    chEXP = EXP[k];
+    EXP[k] = '\0';
+    for (i=0; i<=TAM*2; i++)
+    {
+        if (! compara (EXP, ref[i].nome)) /*SE ELE FOR UM NUMERO*/
+        {
+            int j = 0;
+            if (isdigit (ref[i].valor[0]))
+            {
+                tipoToken = i;
+                while (*EXP && (isalpha (*EXP) || isspace (*EXP)))
+                {
+                    EXP++;
+                }
+                strcat (temp, ref[i].valor);
+                temp[strlen(ref[i].valor)] = '\0';
+                break;
+            }
+            else if (strchr("()+-*/%!", ref[i].valor[0]))
+            {
+                tipoToken = DELIMITADOR;
+                while (*EXP && (isalpha (*EXP) || isspace (*EXP)))
+                {
+                    EXP++;
+                }
+                *temp++ = ref[i].valor[0];
+                *temp = '\0';
+                break;
+            }
+        }
+    }
+}
+
 Ordem* cria_dic (void)
 {
     FILE* nomes = fopen (ARQ_ORDENS, "rt");
@@ -65,6 +179,7 @@ Ordem* cria_dic (void)
         if (fscanf (nomes, "%[^=]=%[^\n]%*c", ref[i].nome, ref[i].valor) >= sizeof(Ordem)) break;
         i++;
     }
+    fclose (nomes);
     return ref;
 }
 
@@ -81,182 +196,49 @@ int compara (char* s1, char* s2) /*relativo a strcmp mas para quando encontra o 
     return s1[i]-s2[i];
 }
 
-char* erroSintaxe (char* a, int cursor)
+void erroSintaxe (int tipoErro)
 {
-    char* erro = (char*) malloc (strlen(a)+120+cursor);
-    if (!erro) ERRO;
-    strcpy (erro, "erro de sintaxe: ");
-    switch (flagErro)
+    FILE* erroS = fopen (ARQ_ERROS, "rb");
+    int temp, i = 0, tamErro;
+    if (! tipoErro)
     {
-        case 1:
-        strcat (erro, "palavra nao faz parte de uma expressao numerica valida.\n\t");
-        break;
-        
-        case 2:
-        strcat (erro, "alguma abertura ou fechamento de parentese esta faltando.\n\t");
-        break;
-    }
-    strcat (erro, a);
-    strcat (erro, "\n\t");
-    int tamErro = strlen(erro), i = 0;
-    while (i<cursor)
-    {
-        erro[tamErro+i] = ' ';
-        i++;
-    }
-    erro[tamErro+i] = '^';
-    erro[tamErro+i+1] = '\n';
-    return erro;
-}
-
-void remEspacosInicio (char* str) /*SE OS ESPAÇO VIEREM NO COMEÇO, COPIE TODA A STRING PARA O COMEÇO*/
-{
-    int i, sp = 0;
-    if (isspace (str[0]))
-    {
-        while (isspace (str[sp+1]))
-            sp++;
-        sp++;
-        for (i=0; str[sp]!='\0'; i++)
+        fscanf (erroS, "%s\n\t", strErro);
+        strcat (strErro, _TEXP);
+        strcat (strErro, "\n\t");
+        temp = EXP - _TEXP;
+        tamErro = strlen (strErro);
+        while (i < temp)
         {
-            str[i] = str[sp];
-            sp++;
-        }
-        str[i] = '\0';
+            strErro[tamErro+i] = ' ';
+            i++;
+        } 
+        strErro[tamErro+i] = '^';
+        strErro[tamErro+i+1] = '\n';
     }
-}
-
-char* remEspacos (char* str) /* SE HOUVEREM ESPAÇOS NO MEIO E NO FIM, RETIRE-OS */
-{
-    char* vazio = (char*) malloc (strlen(str));
-    int i = 0, j = 0;
-    
-    while (str[i] != '\0') 
+    else if (tipoErro == 1)
     {
-        if (str[i] == ' ') 
-        {
-            int temp = i + 1;
-            if (str[temp] != '\0') 
-            {
-                while (str[temp] == ' ') 
-                {
-                    i++;
-                    temp++;
-                }
-            }
-        }
-        vazio[j] = str[i];
-        i++;
-        j++;
+
     }
-    free (str);
-    vazio[j] = '\0';
-    return vazio;
- 
+    else if (tipoErro == 2)
+    {
+
+    }
+    else if (tipoErro == 3)
+    {
+
+        fseek (erroS, 175, SEEK_SET);
+        fscanf (erroS, "%s*c", strErro);
+    }
+    puts (strErro);
+    fclose (erroS);
 }
 
-char* cardinalNumeral (char* exps)
+char* cardinalNumeral (void)
 {
-    Ordem* ref = cria_dic ();
+    ref = cria_dic ();
     int cursor = 0, i = 0, k;
     char* flag = NULL;
-    remEspacosInicio (exps);
-    char* a = remEspacos (exps);
-    char* num = (char*) malloc (strlen(a));
-
-    while(a[cursor]!='\0')
-    {
-        i=0;
-        while (i<TAM*2)
-        {
-            if (! compara (&a[cursor], (char*) "abre ")) 
-            {        
-                a[cursor+4] = '-';
-                flagPos[0].pos++;
-                flagPos[0].cursor = cursor;
-            }
-            else if (! compara (&a[cursor], (char*) "fecha "))
-            {
-                a[cursor+5] = '-';
-                flagPos[1].pos++;
-                flagPos[1].cursor = cursor;
-            }  
-            if (! compara(&a[cursor], ref[i].nome))
-            {
-
-                if (i>=UNIT && i<DEZ)
-                {
-                    flagPos[2].pos++;
-                    flagPos[2].cursor = cursor;
-                }
-                else if (i>=DEZ && i<CENT)
-                {
-                    flagPos[3].pos++;
-                    flagPos[3].cursor = cursor;
-                }
-                else if (i>=CENT && i<MIL)
-                {
-                    flagPos[4].pos++;
-                    flagPos[4].cursor = cursor;
-                }
-                else if (i>=MIL && i<ABRE_P)
-                {
-                    flagPos[5].pos++;
-                    flagPos[5].cursor = cursor;
-                }
-                strcat (num, ref[i].valor);
-                break;
-            }
-            i++;
-        }
-
-
-
-        for (k=0; k<6; k++)
-        {
-            if (flagPos[k].pos > 1)
-            {
-                flagErro = 3;
-                char* erro = (char*) malloc (500);
-                if (!erro) ERRO;
-                strcpy (erro, erroSintaxe (a, (int) flagPos[k].cursor));
-            }
-        }
-
-        if (i == TAM*2)
-        {
-            flagErro = 1;
-            char* erro = (char*) malloc (500);
-            if (!erro) ERRO;
-            strcpy (erro, erroSintaxe (a, cursor));
-            puts (erro);
-            return erro;
-        }
-
-        flag = strpbrk (&a[cursor], " ");
-        if (! flag) break;
-
-        cursor = flag + 1 - a;
-    }
-
-    if (flagPos[0].pos != flagPos[1].pos)
-    {
-        flagErro = 2;
-        char* erro = (char*) malloc (500);
-        if (!erro) ERRO;
-        strcpy (erro, erroSintaxe (a, 0));
-        puts (erro);
-        return erro;
-    }
-
+    char* num = (char*) malloc (strlen(EXP));
+    expAnaliserStart (num);
     return num;
-}
-
-int main (void)
-{
-    char* EXP = (char*) malloc (sizeof (TAM*10));
-    scanf("%[^\n]", EXP);
-    char* res = cardinalNumeral (EXP);
-    puts (res);
-    return 0;
 }
