7cfbf7f5d89449c0fe6cbf9b869c42e8cbac8895
josecleiton <jcleitonbc@gmail.com>
2018-08-21 10:37:04 -0300

    fix

--- lib/preproc.h
+++ lib/preproc.h
@@ -22,12 +22,12 @@ void* MALLOC (size_t size)
 {
     void* ptr;
     ptr = malloc (size);
-    memset(ptr, 0, size);
     if (!ptr)
     {
         fprintf (stderr, "Memoria insuficiente.\n");
         ERRO;
     }
+    memset(ptr, 0, size);
     return ptr;
 }
 
